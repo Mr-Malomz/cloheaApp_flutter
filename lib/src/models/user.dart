0cae194aeecbda159ecf0bcf3f88e998fe86604c
demola malomo <malomo.alaba@yahoo.com>
2021-05-07 19:14:23 +0100

    user model properties

---
+++ lib/src/models/user.dart
@@ -0,0 +1,8 @@
+class User {
+  final String uid;
+  final String firstname;
+  final String lastname;
+  final String email;
+  final int phonenumber;
+  User({this.uid, this.firstname, this.lastname, this.email, this.phonenumber});
+}
